deccd8fc25dc1be52816f3d17a375cb921f1e656
leowu0329 <ryowuandjanet@gmail.com>
2025-07-22 22:26:59 +0800

    完成不良分類、不良狀況、處置對策的欄位(都非必填)，並在前後端建立CRUD的API及樣版

--- models/item.js
+++ models/item.js
@@ -11,12 +11,29 @@ const ItemSchema = new mongoose.Schema(
     salesType: { type: String, enum: ['內銷', '外銷'], required: true },
     customer: { type: String, required: true },
     department: { type: String, required: true },
-    productionOrder: { type: String, required: true, unique: true },
+    productionOrder: { type: String, required: true },
     date: { type: Date, default: Date.now },
     time: { type: String, required: true },
     operator: { type: String, required: true },
     drawingVersion: { type: String, required: true },
     inspector: { type: String, required: true },
+    defects: [
+      {
+        defectCategory: {
+          type: String,
+          enum: [
+            '無圖面',
+            '圖物不符',
+            '尺寸NG',
+            '外觀NG',
+            '人員作業疏失',
+            '特性異常',
+          ],
+        },
+        defectStatus: { type: String },
+        countermeasure: { type: String },
+      },
+    ],
   },
   { timestamps: true },
 );
